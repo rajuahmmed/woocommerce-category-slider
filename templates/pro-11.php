4abf5c285cfd4305d5e4821f93fbb109668071d5
Munir Mahmud <munirmahmud6@gmail.com>
2019-03-18 19:03:32 +0600

    design10 and 11

---
+++ templates/pro-11.php
@@ -0,0 +1,17 @@
+<div class="wcsn-slider wcsn-pro-11">
+	<div class="wcsn-slider-image-wrapper">
+		<a href="#" class="wcsn-slider-link"><img class="wcsn-slider-image" src="<?php echo WC_CATEGORY_SLIDER_ASSETS_URL .'/images/placeholder.jpg';?>" alt=""></a>
+	</div>
+	<div class="wcsn-slider-content-wrapper">
+		<i class="fa fa-bicycle wcsn-slider-icon fa-2x" aria-hidden="true"></i>
+		<a href="#" class="wcsn-slider-link"><h3 class="wcsn-slider-title">Fox Fur Coat</h3></a>
+		<span class="wcsn-slider-product-count">5 products</span>
+		<ul class="wcsn-slider-child-items">
+			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>
+			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (2)</a></li>
+			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>
+		</ul>
+		<p class="wcsn-slider-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eaque!</p>
+		<a href="#" class="wcsn-slider-button">Shop Now</a>
+	</div>
+</div>
