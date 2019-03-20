2d19b222d4498c2a7c5519966f9300d259e7184f
mamunur105 <rmamunur105@gmail.com>
2019-03-20 10:08:29 +0600

    style 25

--- templates/pro-3.php
+++ templates/pro-3.php
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-<div class="wcsn-slider wcsn-pro-3">
-	<div class="wcsn-slider-image-wrapper">
-		<a href="#" class="wcsn-slider-link"><img class="wcsn-slider-image" src="<?php echo WC_CATEGORY_SLIDER_ASSETS_URL .'/images/placeholder.jpg';?>" alt=""></a>
-	</div>
-	<div class="wcsn-slider-content-wrapper">
-		<div class="wcsn-slider-content">
-			<i class="fa fa-bicycle wcsn-slider-icon fa-2x" aria-hidden="true"></i>
-			<a href="#" class="wcsn-slider-link"><h3 class="wcsn-slider-title">Fox Fur Coat</h3></a>
-			<span class="wcsn-slider-product-count">5 products</span>
-			<ul class="wcsn-slider-child-items">
-				<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>
-				<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (2)</a></li>
-				<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>
-			</ul>
-			<p class="wcsn-slider-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eaque!</p>
-			<a href="#" class="wcsn-slider-button">Shop Now</a>
-		</div>
-	</div>
-</div>
-=======
+
 <h3>Pro style 3</h3>
 
 <!-- Pro Demo Style 3-->
@@ -39,6 +19,3 @@
 		<a href="#" class="wcsn-slider-button">Shop Now</a>
 	</div>
 </div>
-
-
->>>>>>> 71423f10070f994cd39951add79b3d11293d89f6
