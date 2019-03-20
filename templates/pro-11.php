84f044a2c93cdf87581e6ae54ecb8e424d8d473e
Munir Mahmud <munirmahmud6@gmail.com>
2019-03-20 08:06:29 +0600

    style 11

--- templates/pro-11.php
+++ templates/pro-11.php
@@ -1,19 +1,19 @@
-<!--<h4>Carousel Style 11</h4>-->
-<!---->
-<!--<div class="wcsn-slider wcsn-pro-11">-->
-<!--	<div class="wcsn-slider-image-wrapper">-->
-<!--		<a href="#" class="wcsn-slider-link"><img class="wcsn-slider-image" src="--><?php //echo WC_CATEGORY_SLIDER_ASSETS_URL .'/images/placeholder.jpg';?><!--" alt=""></a>-->
-<!--	</div>-->
-<!--	<div class="wcsn-slider-content-wrapper">-->
-<!--		<i class="fa fa-bicycle wcsn-slider-icon fa-2x" aria-hidden="true"></i>-->
-<!--		<a href="#" class="wcsn-slider-link"><h3 class="wcsn-slider-title">Fox Fur Coat</h3></a>-->
-<!--		<span class="wcsn-slider-product-count">5 products</span>-->
-<!--		<ul class="wcsn-slider-child-items">-->
-<!--			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>-->
-<!--			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (2)</a></li>-->
-<!--			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>-->
-<!--		</ul>-->
-<!--		<p class="wcsn-slider-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eaque!</p>-->
-<!--		<a href="#" class="wcsn-slider-button">Shop Now</a>-->
-<!--	</div>-->
-<!--</div>-->
+<h4>Carousel Style 11</h4>
+
+<div class="wcsn-slider wcsn-pro-11">
+	<div class="wcsn-slider-image-wrapper">
+    <a href="#" class="wcsn-slider-link"><img class="wcsn-slider-image" src="<?php echo WC_SLIDER_ASSETS_URL .'/images/placeholder.jpg';?>" alt=""></a>
+	</div>
+	<div class="wcsn-slider-content-wrapper" style="background-color: rgba(0, 0, 0, 0.4);">
+		<i class="fa fa-bicycle wcsn-slider-icon fa-2x" aria-hidden="true"></i>
+		<a href="#" class="wcsn-slider-link"><h3 class="wcsn-slider-title">Fox Fur Coat</h3></a>
+		<span class="wcsn-slider-product-count">5 products</span>
+		<ul class="wcsn-slider-child-items">
+			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>
+			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (2)</a></li>
+			<li class="wcsn-slider-child-item"><a href="#" class="wcsn-slider-link">Child item (3)</a></li>
+		</ul>
+		<p class="wcsn-slider-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eaque!</p>
+		<a href="#" class="wcsn-slider-button">Shop Now</a>
+	</div>
+</div>
